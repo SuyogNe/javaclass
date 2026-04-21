be5c1428d830cf14f1eaf71bf20dbdea1c2c9eb9
SuyogNe <suyogn78@gmail.com>
2026-04-21 22:35:32 +0545

    arraysort

---
+++ src/main/java/org/example/sortarray.java
@@ -0,0 +1,18 @@
+package org.example;
+
+
+import java.util.Arrays;
+
+public class sortarray{
+    public static void main(String[] args) {
+
+            int[] numbers = {64, 34, 25, 12, 22, 11, 90};
+
+
+            Arrays.sort(numbers);
+
+
+            System.out.println("Sorted Array: " + Arrays.toString(numbers));
+        }
+    }
+}
